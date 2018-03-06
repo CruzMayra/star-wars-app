1252cafd1a04bae8cdf0fd552292a2f8f91a16a8
CruzMayra <velazquez.leonora@gmail.com>
2018-03-06 10:42:00 -0600

    creación dinámica de tarjetas con nombre de la pelicula

--- js/app.js
+++ js/app.js
@@ -1,64 +1,45 @@
 // función que busca la data y la 'cachea'
 if(!localStorage.getItem('sw-films')) {
-  $.post({
-    url: 'http://graphql.org/swapi-graphql/',
-    data: JSON.stringify({ "{ { allFilms { films { title episodeID characterConnection { characters { name height mass hairColor skinColor } } } } } }" }),
-    contentType: 'application/json'
-  }).done(function(response) {
-    console.log(response);
-    let swData = ('Fetched:', response.data.allFilms);
-    localStorage.setItem('data', JSON.stringify(swData));
-  });
+  $.ajax({
+    url: `https://swapi.co/api/`
+  }).done(getData)
 } else {
-  getPokemonsData(localStorage.getItem('sw-films'))
+  getData(localStorage.getItem('sw-films'))
 }
-
 
 // función que obtiene la data 'cacheada'
-function getFilmsData(data) {
-  let filmsData = JSON.parse(data)
-  console.log(filmsData);
-  // .forEach(pokemon => {
-  //   paintPokemonCard(pokemon);
-  // })
+function getData(data) {
+  $.ajax({
+    url: `${data.films}`
+  }).done(getFilmsData)
 }
 
-// función que pinta en html cada pokemon
-// function paintPokemonCard(pokemon){
-//   let card = '';
-//   card +=
-//   `<section class="col-lg-3 col-8 pokemon-card" data-toggle="modal" data-target="#pokemon-detail" data-id="${pokemon.id}">
-//     <div class="card">
-//       <img class="card-img-top img-fluid rounded mx-auto d-block" src="${pokemon.image}" alt="pokemon-${pokemon.name}">
-//       <div class="card-body">
-//         <h2 class="text-center">${pokemon.name}</h2>
-//       </div>
-//     </div>
-//   </section>`
-//
-//   $('#pokemons-container').append(card)
-// }
-//
-// // función que filtra los pokemones
-// function filterPokemons(){
-//   let searchPokemon = $filterInput.val().toLowerCase();
-//   $('#pokemons-container').empty();
-//   if($filterInput.val().trim().length > 0){
-//     var filteredPokemons = JSON.parse(localStorage.getItem('data')).filter( pokemon => {
-//       let nameMatch = pokemon.name.toLowerCase().indexOf(searchPokemon) >=0
-//       return nameMatch
-//     }).forEach(pokemon => {
-//       paintPokemonCard(pokemon)
-//     })
-//     $('#pokemons-container:empty').html('<p class="h1">Lo sentimos, no encontramos coincidencias <i class="fa fa-frown-o" aria-hidden="true"></i></p>');
-//   } else {
-//     $('#pokemons-container').empty();
-//     JSON.parse(localStorage.getItem('data')).forEach(pokemon => {
-//       paintPokemonCard(pokemon)
-//     })
-//   }
-// }
-//
+function getFilmsData(data){
+  console.log(data);
+  let films = data.results
+  .forEach(film => {
+    paintFilmsCard(film)
+  })
+}
+
+// función que pinta en html cada película
+function paintFilmsCard(films){
+  let card = '';
+  card +=
+  `<section class="col-lg-3 col-8 film-card" data-toggle="modal" data-target="#film-detail" data-id="${films.episode_id}">
+    <div class="card">
+      <div class="card-body">
+        <h2 class="text-center">Name: ${films.title}</h2>
+        <h3 class="text-center">Episode ID: ${films.episode_id}</h3>
+        <ul class="characters-container">
+        </ul>
+      </div>
+    </div>
+  </section>`
+
+  $('#films-container').append(card)
+}
+
 // // función que crea los modales con los datos particulares del pokemon
 // $('#pokemon-detail').on('show.bs.modal', function (event) {
 //   let card = $(event.relatedTarget) // Button that triggered the modal
