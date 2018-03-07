39b7a0348e17a1741d6e2a2a64a2bd2bf77a1ae3
CruzMayra <velazquez.leonora@gmail.com>
2018-03-07 14:59:09 -0600

    corección de la función que muestras los personajes de cada película

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ if(!localStorage.getItem('sw-data')) {
     data: JSON.stringify({ "query": " { allFilms { films { title episodeID characterConnection { characters { name height mass hairColor skinColor } } } } } " }),
     contentType: 'application/json'
   }).done(function(response) {
-    // console.log(response);
     let swData = ('Fetched:', response.data.allFilms.films);
     localStorage.setItem('sw-data', JSON.stringify(swData));
   });
@@ -15,6 +14,7 @@ if(!localStorage.getItem('sw-data')) {
 
 // función que obtiene la data 'cacheada'
 function getSwData(data) {
+  // console.log(data);
   let filmsData = JSON.parse(data)
   .forEach(film => {
     paintFilmsCard(film)
@@ -23,6 +23,12 @@ function getSwData(data) {
 
 // // función que pinta en html cada película
 function paintFilmsCard(film){
+  let templateCharacter = '';
+  film.characterConnection.characters.forEach( item => {
+    templateCharacter +=
+    `<li data-toggle="modal" data-target="#character-detail" data-id="${item.name}">${item.name}</li>`
+  })
+
   let card = '';
   card +=
   `<section class="col-lg-6 col-6 film-card">
@@ -32,65 +38,22 @@ function paintFilmsCard(film){
         <h3 class="text-center">Episode ${film.episodeID}</h3>
         <h3 class="text-center">Characters:</h3>
         <ul class="characters-container">
+        ${templateCharacter}
         </ul>
       </div>
     </div>
   </section>`
 
   $('#films-container').append(card)
-  getCharacters(film.characterConnection.characters)
 }
 
-// función que itera sobre el arreglo que contiene los personajes
-function getCharacters(array) {
-  console.log(array);
-  return array.forEach(item => {
-    let character = ``
-    character += `<li data-toggle="modal" data-target="#character-detail" data-id="${item.name}">${item.name}</li>`
-    $('.characters-container').append(character)
+// función que crea los modales con los datos particulares del personaje
+$('#character-detail').on('show.bs.modal', function (event) {
+  let card = $(event.relatedTarget) // Button that triggered the modal
+  let character = JSON.parse(localStorage.getItem('sw-data')).find(item => {
+    return card.data('id')
   })
-}
-
-// // función que crea los modales con los datos particulares del pokemon
-// $('#pokemon-detail').on('show.bs.modal', function (event) {
-//   let card = $(event.relatedTarget) // Button that triggered the modal
-//   let pokemon = JSON.parse(localStorage.getItem('data')).find( pokemon => {
-//     return pokemon.id === card.data('id')
-//   })
-//   var modal = $(this)
-//   modal.find('.modal-title').text(pokemon.name);
-//   modal.find('.modal-img').attr('src',pokemon.image);
-//   modal.find('.classification').text(pokemon.classification);
-//   modal.find('.type').empty().text(createTypesButtons(pokemon.types));
-//   modal.find('.height').text(pokemon.height.maximum);
-//   modal.find('.weight').text(pokemon.weight.maximum);
-//   modal.find('.resistant').empty().text(createResistantButtons(pokemon.resistant));
-//   modal.find('.weaknesses').empty().text(createWeaknessesButtons(pokemon.weaknesses));
-// })
-//
-// // función que crea botones 'type' del modal
-// function createTypesButtons(array){
-//   return array.forEach(item => {
-//     var templateButton = '';
-//     templateButton += `<button type="button" class="btn btn-sm ${item}">${item}</button>`
-//     $('.type').append(templateButton)
-//   })
-// }
-//
-// // función qye crea botoenes 'resistant' del modal
-// function createResistantButtons(array){
-//     return array.forEach(item => {
-//       var templateButton = '';
-//       templateButton += `<button type="button" class="btn btn-sm ${item}">${item}</button>`
-//       $('.resistant').append(templateButton)
-//     })
-// }
-//
-// // función que crea los botones 'weaknesses' del modal
-// function createWeaknessesButtons(array){
-//     return array.forEach(item => {
-//       var templateButton = '';
-//       templateButton += `<button type="button" class="btn btn-sm ${item}">${item}</button>`
-//       $('.weaknesses').append(templateButton)
-//     })
-// }
+  console.log(character);
+  let modal = $(this);
+  modal.find('.name').text(character.name);
+})
